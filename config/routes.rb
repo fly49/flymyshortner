c39dd19ac5dc34c814f7304767d1c7185972bfb4
flymypie <flymaipie@gmail.com>
2018-10-17 18:03:29 +0000

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  root 'home#index'
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+
+  root 'links#index'
+  get '/:short_url' => 'links#show'
+  post 'create_link' => 'links#create'
+  resources :links, only: %i[index show create]
 end
