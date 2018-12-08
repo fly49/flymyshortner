7991c3bc5cf622e9080f7271d21d6f32c9d065e4
flymypie <flymaipie@gmail.com>
2018-12-08 19:59:47 +0000

    try sidekiq work on deploy

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDISTOGO_URL'], size: 2 }
+  config.redis = { url: ENV['REDISTOGO_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDISTOGO_URL'], size: 2 }
+  config.redis = { url: ENV['REDISTOGO_URL'] }
 end
