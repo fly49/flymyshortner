d3fc8c17fa06237ec7bcfdff53fd04c98287c4d3
flymypie <flymaipie@gmail.com>
2018-12-09 10:05:53 +0000

    fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDISTOGO_URL'] }
+  config.redis = { url: ENV['REDIS_PROVIDER'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDISTOGO_URL'], size: 5 }
+  config.redis = { url: ENV['REDIS_PROVIDER'], size: 5 }
 end
